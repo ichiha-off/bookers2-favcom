ce1e489225c68d9727bd1d6ed2e7735315efd460
vagrant <vagrant@localhost.localdomain>
2020-07-02 07:38:09 +0000

    [Fav]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'homes#top' 
   get 'home/about' => 'homes#about'
 
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
 end
